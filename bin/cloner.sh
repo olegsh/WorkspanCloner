21632563b5ef7893f0d06469bea2eb2f2eea5b2c
Oleg S <tanoleg@gmail.com>
2019-09-05 10:54:09 -0700

    add execution script

--- bin/cloner.sh
+++ bin/cloner.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+### input json file name
+arg1=$1
+### input start index Entinty
+arg2=$2
+
+### build directory
+dir=./target
+## jar filename
+jarfile=${dir}/qv2-1.0.0.jar
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo "Missing arguments, exiting..."
+    echo "Usage: $0 input_file_name start_index_entity"
+    exit 1
+fi
+
+java -jar ${jarfile} ${arg1} ${arg2}
